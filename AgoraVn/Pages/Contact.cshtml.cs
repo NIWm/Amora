ae3427361a5c08324b4f3a6e880408e2575fb992
Quynh Doan <doanq@sysmex.com>
2023-11-16 01:14:30 +0700

    Add new

---
+++ AgoraVn/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AmoraVN.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AgoraVn/Pages/Why.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AmoraVN.Pages
+{
+    public class WhyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
